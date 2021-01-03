660d12752639e8e6a1eb1411d3b2090358735cc5
Rob Desktop <rob@rob-leach.net>
2021-01-03 09:32:19 -0600

    Basic tick with setInterval()
    Start with the basics, gotta get something to iterate on.

--- client/src/App.js
+++ client/src/App.js
@@ -14,19 +14,11 @@ class App extends Component {
   componentWillUnmount() {
   }
 
-  tick() {
-    
-  }
-
-  producerBacklogAdd = () => {
-	}
-
   render () {
   	return (
       <div className="App">
 		    <h1>k-sim: Kafka Failover/Throughput Simulator</h1>
 				<p>A simple simulator designed to explore bottlenecking and throughput scenarios.  Written with Kafka fundametnals in mind, there's no reason why this shouldn't apply to other queuing technologies.</p>
-			  <button onClick={this.producerBacklogAdd(10)}>sudden producer backlog (10)</button>
 			  <Simulator />
 			</div>
     );
--- client/src/Simulator.js
+++ client/src/Simulator.js
@@ -8,6 +8,10 @@ class Simulator extends React.Component {
 		super();
 		this.state = {
       tickNumber: 0,
+			maxTicks: 100,
+			running: false,
+			tickIntervalId: null,
+			tickMs: 750,
       producers: [ { 
           producerId: 0,
           backlog: 1000,
@@ -31,12 +35,33 @@ class Simulator extends React.Component {
 		};
   }
 
+	componentDidMount() {
+		// should we roll this into the setState statement?
+		var tickIntervalId = setInterval(
+			() => this.tick(),   // It is unclear to me why I needed the () => here... but it only works that way!
+			this.state.tickMs);  
+		this.setState({
+			...this.state,
+			tickIntervalId: tickIntervalId,
+			running: true
+		})
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.state.tickIntervalId) // Clean up our mess
+	}
+
   tick() {
+		this.setState({
+			...this.state,
+			tickNumber: this.state.tickNumber + 1
+		})
   }
 
 	render() {
 		return(
 			<div className="kSim">
+				<div className="ticker"> {this.state.tickNumber}/{this.state.maxTicks} run:{this.state.running} </div>
         <Producer backlog={this.state.producers[0].backlog} />
         <Partition maxOffset={this.state.partitions[0].maxOffset} />
         <Consumer currentOffset={this.state.consumers[0].currentOffset} />
