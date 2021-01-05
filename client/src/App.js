a90358acf0c0037e50aea25a5efb78d517f50445
Rob.Leach <rob.leach@target.com>
2021-01-04 19:59:21 -0600

    set up settings, use tomorrow

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,20 @@ import Simulator from './Simulator.js';
 class App extends Component {
 	constructor(props) {
 	  super(props)
-		this.state = { }
+		this.state = { 
+			settings: {
+				producer: { backlog: 0, createRate: 3, produceRate: 3 },
+				partition: { maxReceiveRate: 10, maxTransmitRate: 10 },
+				consumer: { consumeRate: 5 },
+				partitionBalanceStrategy: 'round-robin',
+				showSettings: true
+			},
+			layout: {
+				numProducers: 5,
+				numPartitions: 5,
+				numConsumers: 4
+			}
+		}
 	}
   componentDidMount() {
     
@@ -19,7 +32,12 @@ class App extends Component {
 		<div className="App">
 		    <h1>k-sim: Kafka Failover/Throughput Simulator</h1>
 			<p>A simple simulator designed to explore bottlenecking and throughput scenarios.  Written with Kafka fundametnals in mind, there's no reason why this shouldn't apply to other queuing technologies.</p>
-			<Simulator {...{numProducers: 5, numPartitions: 5, numConsumers: 4}} />
+			<Simulator {...{
+				numProducers: this.state.layout.numProducers, 
+				numPartitions: this.state.layout.numPartitions, 
+				numConsumers: this.state.layout.numConsumers,
+				settings: this.state.settings
+			}} />
 		</div>
     );
   }
